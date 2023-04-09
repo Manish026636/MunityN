0692e68317363b5d67f2f96ec0c7aa40e613fd9c
Manish <my79220@gmail.com>
2023-04-09 18:13:48 +0530

    sdjhfd

--- src/components/common/NavHead.jsx
+++ src/components/common/NavHead.jsx
@@ -26,7 +26,7 @@ import {
 
   FaBars,
 } from "react-icons/fa";
-import Gslsec from "./Gslsec";
+import Dadd from "./Dadd";
 
 // profile menu component
 const profileMenuItems = [
@@ -257,7 +257,7 @@ const NavHead = () => {
         </div>
         
       </div>
-      <Gslsec/>
+      <Dadd/>
 
       {!tab && <CPDashboard />}
 
