1907bc890c3329f2b9d862cdb9ed91b1ce88e5fd
Manish <my79220@gmail.com>
2023-04-09 12:11:41 +0530

    done

--- src/components/common/NavHead.jsx
+++ src/components/common/NavHead.jsx
@@ -1,14 +1,186 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
+import CPDashboard from '../chairperson/CPDashboard';
 
 import {
   Navbar,
   MobileNav,
   Typography,
   Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Card,
   IconButton,
 } from "@material-tailwind/react";
-import CPDashboard from '../chairperson/CPDashboard';
+import {
+  FaCube,
+  FaUserCircle,
+  FaCode,
+  FaChevronDown,
+  FaCog,
+  FaInbox,
+  FaLifeRing,
+  FaPowerOff,
+  FaRocket,
+  FaBars,
+} from "react-icons/fa";
+
+// profile menu component
+const profileMenuItems = [
+  {
+    label: "My Profile",
+    icon: FaUserCircle,
+  },
+  {
+    label: "Edit Profile",
+    icon: FaCog,
+  },
+  {
+    label: "Inbox",
+    icon: FaInbox,
+  },
+  {
+    label: "Help",
+    icon: FaLifeRing,
+  },
+  {
+    label: "Sign Out",
+    icon: FaPowerOff,
+  },
+];
+
+function ProfileMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="blue-gray"
+          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+        >
+          <Avatar
+            variant="circular"
+            size="sm"
+            alt="candice wu"
+            className="border border-blue-500 p-0.5"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+          />
+          <FaChevronDown
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+      <MenuList className="p-1">
+        {profileMenuItems.map(({ label, icon }, key) => {
+          const isLastItem = key === profileMenuItems.length - 1;
+          return (
+            <MenuItem
+              key={label}
+              onClick={closeMenu}
+              className={`flex items-center gap-2 rounded ${
+                isLastItem
+                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                  : ""
+              }`}
+            >
+              {React.createElement(icon, {
+                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                strokeWidth: 2,
+              })}
+              <Typography
+                as="span"
+                variant="small"
+                className="font-normal"
+                color={isLastItem ? "red" : "inherit"}
+              >
+                {label}
+              </Typography>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
+    </Menu>
+  );
+}
+
+
+
+function NavListMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const triggers = {
+    onMouseEnter: () => setIsMenuOpen(true),
+    onMouseLeave: () => setIsMenuOpen(false),
+  };
+
+  
+
+  return (
+    <React.Fragment>
+      <Menu open={isMenuOpen} handler={setIsMenuOpen}>
+        <MenuHandler>
+          <Typography as="a" href="#" variant="small" className="font-normal">
+            
+          </Typography>
+        </MenuHandler>
+        
+          
+            
+      </Menu>
+      
+      
+    </React.Fragment>
+  );
+}
+
+// nav list component
+const navListItems = [
+  {
+    label: "Account",
+    icon: FaUserCircle,
+  },
+  {
+    label: "Blocks",
+    icon: FaCube,
+  },
+  {
+    label: "Docs",
+    icon: FaCode,
+  },
+];
+
+function NavList() {
+
+  return (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+      <NavListMenu />
+      {navListItems.map(({ label, icon }, key) => (
+        <Typography
+          key={label}
+          as="a"
+          href="#"
+          variant="small"
+          color="blue-gray"
+          className="font-normal"
+        >
+          <MenuItem className="flex items-center gap-2 lg:rounded-full">
+            {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
+            {label}
+          </MenuItem>
+        </Typography>
+      ))}
+    </ul>
+  );
+}
 
 const NavHead = () => {
   const tabs = [
@@ -30,128 +202,78 @@ const NavHead = () => {
   }, [tab]);
 
     const [openNav, setOpenNav] = React.useState(false);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="/" className="flex items-center">
-        <h1>Profile😎Icon</h1>
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="/" className="flex items-center">
-         Name
-        </a>
-      </Typography>
-      
-    </ul>
-  );
- 
+
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setIsNavOpen(false)
+    );
+  }, []);
+
   return (
     <>
-    <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
-        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+   <Navbar className="mx-auto max-w-screen-4xl p-2 lg:pl-6">
+        <div className="relative mx-auto flex items-center text-blue-gray-900">
           <Typography
             as="a"
             href="#"
-            className=" cursor-pointer  font-bold"
+            className="mr-4 w-[80%] ml-2 cursor-pointer py-1.5 font-medium"
           >
-           <img src="Mu.png" width="80px" alt=""/>
+            Material Tailwind
           </Typography>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <Button
-              
-              size="md"
-              className="hidden bg-[#FF5A82] lg:inline-block"
-            >
-              <span>Logout</span>
-            </Button>
+          <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
+            <NavList />
+          </div>
+          <div className="w-full flex justify-end items-center">
+            <ProfileMenu />
             <IconButton
+              size="sm"
+              color="blue-gray"
               variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
+              onClick={toggleIsNavOpen}
+              className="mr-2 lg:hidden"
             >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
+              <FaBars className="h-6 w-6" />
             </IconButton>
           </div>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <Button  size="md" fullWidth className="bg-[#FF5A82] mb-2">
-            <span>Logout</span>
-          </Button>
+        <MobileNav open={isNavOpen} className="overflow-scroll">
+          <NavList />
         </MobileNav>
       </Navbar>
-      <div className="bg-gray-100">
-      <div className="flex justify-center items-center mt-10">
-      <div className="bg-gradient-to-r from-cyan-200 to-indigo-100 rounded-xl p-2">
-        {tabs.map(tab => (
-          <button
-            key={tab.id}
-            className={`py-3 bg-white hover:opacity-90 rounded-xl px-4 md:px-12 text-sm font-bold m-2  text-gray-700 border-b-2${
-              currentTab === tab.id ? 'border-green-500 text-white   bg-blue-300' : 'border-gray-800 '
-            }`}
-            onClick={() => navigate(`/dashboard/${tab.path}`)}
-          >
-            {tab.label}
-          </button>
-        ))}
-      </div>
-</div>
+<div className="bg-gray-100">
+  <div className="flex justify-center items-center mt-10">
+    <div className="bg-gradient-to-r from-cyan-200 to-indigo-100 rounded-xl p-2">
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          className={`py-3  hover:opacity-90 rounded-xl px-4 md:px-12 text-sm font-bold m-2  text-gray-700 border-b-2${
+            currentTab === tab.id
+              ? "border-green-500 bg-blue-300 text-white   "
+              : "border-gray-800 bg-white "
+          }`}
+          onClick={() => navigate(`/dashboard/${tab.path}`)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  </div>
 </div>
    {!tab && <CPDashboard/>}
-      
+
       </>
   )
 }
 
-export default NavHead
+export default NavHead;
